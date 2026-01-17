2cc5c09e886c7663fbb2c4dc839dd8e95af4a222
Your Name <your.email@example.com>
2026-01-17 23:07:51 +0530

    created position service

---
+++ src/main/java/com/example/demo/Service/PositionServe.java
@@ -0,0 +1,15 @@
+
+package com.example.demo.Service;
+import com.example.demo.model.Employee;
+import com.example.demo.Repository.EmployeeRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class PositionServe {
+    
+}
